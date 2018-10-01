a9240e330a10c61eabaec5b843ad26e4c86a03bd
larissakouete <43169260+larissakouete@users.noreply.github.com>
2018-10-01 23:11:48 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :pizzas
   post 'orders/creer_commande', to: 'orders#create'
-  resources :orders/lister_commande
+  get 'orders/lister_commande'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
